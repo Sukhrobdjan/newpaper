8f17baa36b8de29bee9d376a045bc38e10f289a6
Suxrob <salomsuxrob@gmail.com>
2022-05-13 01:40:07 +0500

    Dasturni asosi tayyorlanmoqada

--- newspaper/blog/models.py
+++ newspaper/blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
-
+ 
 class Post(models.Model):
     title = models.CharField(max_length=40)
     text = models.TextField(max_length=200)
+    # author = models.ForeignKey(
+    #     'auth.User',
+    #     on_delete=models.CASCADE
+    # )
 
     def __str__(self):
         return self.title
--- newspaper/newspaper/settings.py
+++ newspaper/newspaper/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # local 
+    # local apps
     'blog.apps.BlogConfig',
 ]
 
